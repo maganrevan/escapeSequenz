2c7576eb5c75796a62705be051eee0c7c71cf3a5
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2018-06-18 22:12:45 +0200

    Initial Commit

---
+++ escapeSequenz/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  escapeSequenz
+//
+//  Created by Magnus Kruschwitz on 18.06.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
